9654a71e9efad0d430204c53a71e08d6376794cc
Kraichik <romanfjxxxh@gmail.com>
2022-11-23 23:16:30 +0200

    Add project files.

---
+++ Find the odd int/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Find_the_odd_int
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 3, 3, 3, 3, 2, 2, 1 };
+            Console.WriteLine(find_it(ints));
+            Console.WriteLine(find_it_linq(ints));
+            Console.ReadLine();
+
+        }
+        public static int find_it_linq(int[] seq)
+        {
+            return seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
+        }
+        public static int find_it(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count % 2 != 0)
+                {
+                    return array[i];
+                }
+            }
+            return -1;
+        }
+    }
+}
